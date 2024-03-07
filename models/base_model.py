736833f011c163323f492b38792b51136c1b453d
MucheruM <petermucheru420@gmail.com>
2024-03-06 22:25:42 -0800

    Modifyed the updated_at attribute to self.created_at

--- models/base_model.py
+++ models/base_model.py
@@ -15,7 +15,7 @@ class BaseModel:
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        self.updated_at = self.created_at
 
     def __str__(self):
         """
