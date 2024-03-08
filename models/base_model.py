19673b6cc6897e7e74c4f057628b3df98d564724
nhcimunyascripts <nchimunya.backlogcode@gmail.com>
2024-03-08 07:30:22 +0200

    removed duplicates

--- models/base_model.py
+++ models/base_model.py
@@ -13,17 +13,12 @@ class BaseModel:
         This means that all instances created from this class,
         will have all the attributes and values describes here
         """
-
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = self.created_at
-
         if kwargs:
-	# self.__dict__ = kwargs
-	# self.created_at = datetime.strptime(self.created_at, "
-	# %Y-%m-%dT%H:%M:%S.%f")
-	# self.updated_at = datetime.strptime(self.updated_at, "
-	# %Y-%m-%dT%H:%M:%S.%f")
+            # self.__dict__ = kwargs
+            # self.created_at = datetime.strptime(self.created_at, "
+            # %Y-%m-%dT%H:%M:%S.%f")
+            # self.updated_at = datetime.strptime(self.updated_at, "
+            # %Y-%m-%dT%H:%M:%S.%f")
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
@@ -38,13 +33,13 @@ class BaseModel:
         """
         Returns a human-readable/informal, str repr of an obj
         Returns:
-            dict: The object attributes such as id and time of 
+            dict: The object attributes such as id and time of
                   BaseModel and their values
         """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Update the update at time attribute with the current tumestamp"""   
+        """Update the update at time attribute with the current tumestamp"""
         self.updated_at = datetime.utcnow()
 
     def to_dict(self):
